697b33a068b19ef43fd7ab844b8cf6a16a52c31f
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-12-15 01:17:32 +0200

    added logger

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -1,14 +1,16 @@
 package mate.service;
 
+import java.util.Optional;
 import mate.exception.AuthenticationException;
 import mate.lib.Inject;
 import mate.lib.Service;
 import mate.model.Driver;
-
-import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationService.class);
     @Inject
     private DriverService driverService;
 
@@ -16,8 +18,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> driver = driverService.findByLogin(login);
         if (driver.isPresent() && driver.get().getPassword().equals(password)) {
+            logger.info("login method was called. Params: login = {}", login);
             return driver.get();
         }
-        throw new AuthenticationException("Login or password was incorrect");
+        logger.error("Login or password was incorrect");
+        throw new AuthenticationException("Login or password was incorrect;");
     }
 }
